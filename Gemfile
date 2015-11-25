cfd8e80f20f8ca3a4b34f10fb1f43f1104b7c5f9
jnguyenvnn <james.nguyen.vnn@gmail.com>
2015-11-25 19:53:22 +0700

    added more test for user model

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ group :test do
   gem 'rspec-rails'
   gem 'factory_girl_rails'
   gem 'ffaker'
+  gem 'shoulda-matchers'
 end
 
 # To use ActiveModel has_secure_password
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,14 +8,19 @@ RSpec.describe User, type: :model do
   it { should respond_to(:name) }
   it { should respond_to(:email) }
 
-  it { should be_valid }
+
 
   describe 'when email is not present' do
     before { @user.email = '' }
     it { should_not be_valid }
   end
 
-
+  describe 'email should be unique' do
+    @older_user = FactoryGirl.create(:user)
+    @new_user = FactoryGirl.build(:user)
+    @new_user.email = @older_user.email
+    it { should_not be_valid }
+  end
 end
 
 
