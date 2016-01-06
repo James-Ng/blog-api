b52412aa75752d1d9f8d13c07d1d5536b9ec890e
jamesnguyenvnn <james.nguyen.vnn@gmail.com>
2016-01-06 18:00:43 +0700

    commit 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
 
   def generate_auth_token
     begin
+    	p 'test 1'
       self.auth_token = SecureRandom.hex
     end while self.class.exists?(auth_token: auth_token)
   end
