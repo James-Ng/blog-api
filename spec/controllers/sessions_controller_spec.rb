ef0eaebb780ebd5c81aad90b37ee85120d27132a
jamesnguyenvnn <james.nguyen.vnn@gmail.com>
2016-01-06 23:47:59 +0700

    add comment 13 #12

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -23,6 +23,7 @@ RSpec.describe SessionsController, type: :controller do
     before(:each) do
       p "isse 12"
       p "isse 13"
+      p "isse 14"
       @user = FactoryGirl.create :user
       delete :destroy, id: @user.auth_token
     end
