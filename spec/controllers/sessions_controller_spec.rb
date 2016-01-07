ccf3a8eefd5cf2fa43acc7694593c52499ba25cb
jamesnguyenvnn <james.nguyen.vnn@gmail.com>
2016-01-07 10:46:14 +0700

    make it conflict

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -21,14 +21,14 @@ RSpec.describe SessionsController, type: :controller do
   describe "DELETE #destroy" do
 
     before(:each) do
-      p "isse 12"
+
       p "isse 13"
       p "isse 14"
       p "close issue 12"
       @user = FactoryGirl.create :user
       delete :destroy, id: @user.auth_token
     end
-
+    p 'lajsdkjkas'
     it { should respond_with 204 }
 
   end
